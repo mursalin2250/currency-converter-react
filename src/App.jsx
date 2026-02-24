b65d4a19999befed734364049c61fc4889e16018
mursalin2250 <mursalin2250@gmail.com>
2026-02-24 14:43:02 +0600

    create input box

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 import InputBox from './components/InputBox'
 
 function App() {
+	const count = useRef(0);
+	const [cnt, setCnt] = useState(0);
 
-
+	const setCount = () => {
+		count.current += 1;
+		setCnt(() => count.current);
+		console.log(count.current)
+	}
 	return (
 		<>
 			<main className='h-screen flex items-center justify-center' style={{ backgroundImage: "url(/bg-img.png)" }}>
-				<div className='bg-white px-20 py-15'>
+				<div className='bg-green-800 px-20 py-15 flex flex-col gap-10 rounded-lg'>
 					<div>
-						<h1 className='text-5xl text-green-700'>Currency Converter</h1>
+						<h1 className='text-5xl text-white'>Currency Converter</h1>
 					</div>
 					<form action="POST">
 						<InputBox />
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 
 const InputBox = () => {
-  return (
-    <div>
-        <input type="number" value="" placeholder='Numbers'className='outline-none'/>
-    </div>
-  )
+    return (
+        <div className='bg-white p-4 flex flex-col gap-3 rounded-md'>
+            <div className='flex justify-between'>
+                <legend className=" text-md text-neutral-400">From</legend>
+                <legend className=" text-md text-neutral-400">Currency Type</legend>
+            </div>
+            <div className='flex justify-between items-center'>
+                <input type="text" placeholder="Type here" className="input px-0 bg-white text-black placeholder:text-gray-400" />
+                <div class="dropdown dropdown-end">
+                    <div tabindex="0" role="button" class="btn text-black bg-neutral-200 border-0 shadow-none">Item</div>
+                    <ul tabindex="-1" class="dropdown-content menu bg-white rounded-box z-1 w-52 p-2">
+                        <li className='text-black'><a>Item 1</a></li>
+                        <li className='text-black'><a>Item 1</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default InputBox
